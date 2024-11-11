d020656988b483f7e7c219b84c82379a3c6ee350
ARATA Mizuki <minorinoki@gmail.com>
2024-11-11 22:50:40 +0900

    docs: Exclude .venv

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ author = 'ARATA Mizuki'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 
 
 
